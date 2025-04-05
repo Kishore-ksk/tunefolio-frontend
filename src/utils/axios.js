6f2e633441e906a0febbd000668d9d4fbc6353cb
Kishore-ksk <kishoreshanjay2000@gmail.com>
2025-04-05 15:18:56 +0530

    Initial commit: Tunefolio frontend

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create Axios instance
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000', // Laravel backend URL
-  withCredentials: false, // Allows sending cookies with requests (for Sanctum auth)
+  withCredentials: true, // Allows sending cookies with requests (for Sanctum auth)
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
